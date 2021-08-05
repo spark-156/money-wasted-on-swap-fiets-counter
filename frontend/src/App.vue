7c640ecf68ca92d7acf975615c34b715bf595163
spark-156 <lucabergman@hotmail.nl>
2021-08-05 22:55:48 +0200

    Counter for amount of euros spent on swap bike

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,50 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  <div>{{ name }}</div>
+    <img >
+    <!-- <HelloWorld/> -->
+    <div>Hoeveel geld heeft u aan swapfiets verspilt?</div>
+    <br/>
+  <div>{{ money }} €</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  },
   data () {
     return {
-      name: 'ruben doorakkers'
+      money: 0,
+      monthlyBill: 13.5,
+      startDate: '2020-2-11'
     }
+  },
+  computed: {
+  },
+  mounted: function () {
+    window.setInterval(() => {
+      this.money = ((Date.now() - new Date(this.startDate).getTime()) * this.monthlyBill / 1000 / 60 / 60 / 24 / 30.5).toFixed(6)
+    }, 1000)
   }
 }
 </script>
 
 <style>
+html, body {
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  margin: 10vh 0;
+  height: 80vh;
+  background-image: url("./assets/ruben.jpeg");
+  background-repeat:no-repeat;
+  background-position: center center;
+  background-size: cover;
 }
 </style>
